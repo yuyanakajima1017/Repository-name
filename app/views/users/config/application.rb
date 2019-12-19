c358c9ff8d698bc228d6f07de1e080e1d0da7ed8
yuya <yuya@gmail.com>
2019-12-19 08:40:43 +0000

    Finish editing-users

---
+++ app/views/users/config/application.rb
@@ -0,0 +1,18 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module AttendanceApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+    config.time_zone = 'Asia/Tokyo' # この１行をここに追加します。
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
